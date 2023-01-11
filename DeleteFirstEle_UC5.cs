6f12a2050d23a251732f769c8f935b009c3afb5c
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2023-01-11 15:52:54 +0530

    Add files via upload

---
+++ DeleteFirstEle_UC5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class DeleteFirstEle_UC5
+    {
+        public void DeleteEle()
+        {
+            int[] num = { 56, 30, 70 };
+            LinkedList<int> myList = new LinkedList<int>(num);
+
+            foreach(var n in myList)
+            {
+                Console.WriteLine(n);
+            }
+
+            myList.RemoveFirst();
+            Console.WriteLine("Linked after removing the first node");
+
+            foreach(var n in myList)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
---
+++ DeleteNode_UC9.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class DeleteNode_UC9
+    {
+        public Node head;
+
+        public void push(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = head;
+            head = new_node;
+
+        }
+
+        public void DeleteNode(int key)
+        {
+            Node temp = head, prev = null;
+
+            if(temp != null && temp.data == key) 
+            {
+                head = temp.next;
+                return;
+            }
+            while(temp != null && temp.data != key)
+            {
+                prev = temp; 
+                temp=temp.next;
+            }
+            if(temp== null)
+            {
+                return;
+            }
+            prev.next = temp.next;
+                
+        
+        }
+        public void printList()
+        {
+            Node tnode = head;
+            while (tnode != null)
+            {
+                Console.WriteLine(tnode.data + " ");
+                tnode = tnode.next;
+
+            }
+        }
+    }
+}
